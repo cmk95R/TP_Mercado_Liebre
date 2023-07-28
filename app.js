73d9b129b59db7558b751c33e526edf71e248eaa
Cristian Velazquez <cfvhca@gmail.com>
2023-07-28 10:14:49 -0300

    Se agrego login y register - TP Formulario

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const path = require('path');
 const PORT = 3030;
 
 /* configuraciones */
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('./public'));
+app.use(express.static('./views'));
 
 /* rutas */
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
-app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'views','404.html')));
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'/views/register.html')));
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'/views/login.html')));
+app.get('/*',(req,res) => res.sendFile(path.join(__dirname,'/views/404.html')));
 
 
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
